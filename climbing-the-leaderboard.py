e915e12b936fe4f64b77f9f21238960dfa1781c6
bridgeandcities <dakaizhou.gz@gmail.com>
2019-08-21 11:56:09 +1200

    Create climbing-the-leaderboard.py

---
+++ climbing-the-leaderboard.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the climbingLeaderboard function below.
+def climbingLeaderboard(scores, alice):
+    #print(alice)
+    scores = sorted(list(set(scores)), reverse=True)
+    #print(scores)
+    lenScores = len(scores)
+    result = []
+    for aliceScore in alice:
+        #print("alice's score: ", aliceScore)
+        for i in range(len(scores)):
+            #print("place: ", i+1, "Score: ", scores[i], lenScores)
+            if aliceScore >= scores[i]:
+                #print(i+1)
+                result.append(i+1)
+                break
+            if i+1 == lenScores:
+                #print(i+2)
+                result.append(i+2)
+                break
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    scores_count = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    alice_count = int(input())
+
+    alice = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(scores, alice)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
